2f47666f1c7c1c03e6a78624123646f0999318d3
Raúl González <gonzalv@ciencias.unam.mx>
2021-01-28 21:01:10 -0600

    Agrega visualizacion de contenido de fatura (items, cliente, etc)

--- src/components/invoices/Invoice.js
+++ src/components/invoices/Invoice.js
@@ -20,6 +20,57 @@ export default class Invoice extends React.Component {
 			});
 		}
 	}
+
+	createContent(props) {
+		const customer = props.customer;
+		const payedWithCard = props.paymentMethod === 'CARD';
+		return(
+			<div className="row">
+				<div className="col-6">
+					<p><b>Número de factura:&nbsp;</b> {props.numberInvoice}</p>
+					<p><b>Fecha de facturación:&nbsp;</b> {props.createAt}</p>
+					<p><b>Descripción:&nbsp;</b> {props.description}</p>
+					<b>Cliente:&nbsp;</b>
+					<ul>
+						<li>{customer.firstName}&nbsp;{customer.lastName}</li>
+						<li>{customer.numberID}</li>
+						<li>{customer.email}</li>
+					</ul>
+				</div>
+				<div className="col-6">
+					<p><b>Método de pago:&nbsp;</b> {payedWithCard ? 'Tarjeta' : 'Efectivo'}</p>
+					{payedWithCard ? (<p><b>Tarjeta:&nbsp;</b> {'*'.repeat(12) + props.cardLastDigits}</p>) : null}
+					<p><b>Total:&nbsp;</b> {'$' + props.subTotal}</p>
+					{this.createItemsTable(props.items)}
+				</div>
+			</div>
+		);
+	}
+
+	createItemsTable(items) {
+		return(
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th scope="col">Producto</th>
+						<th scope="col">Precio</th>
+						<th scope="col">Cantidad</th>
+						<th scope="col">Subtotal</th>
+					</tr>
+				</thead>
+				<tbody>
+					{items.map(item => (
+						<tr key={item.id}>
+							<td>{item.product.name}</td>
+							<td>{`$${item.price}`}</td>
+							<td>{item.quantity}</td>
+							<td>{`$${item.subTotal}`}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		);
+	}
 	
 	render () {
 		return (
@@ -34,7 +85,7 @@ export default class Invoice extends React.Component {
 						aria-controls={`collapse${this.state.id}`}
 						onClick={() => this.getFullData()}
 					>
-						Fatura #{this.state.numberInvoice}
+						Fatura #{this.state.numberInvoice} ({this.state.createAt})
 					</button>
 					<button
 						type="button"
@@ -52,7 +103,7 @@ export default class Invoice extends React.Component {
 					data-parent={`#${this.state.parentId}`}
 				>
 					<div className="card-body">
-						{this.state.content}
+						{this.state.customer ? this.createContent(this.state) : null}
 					</div>
 				</div>
 			</div>
